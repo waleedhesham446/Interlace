b21d162e1243bde0861929055a9dd7a3c26033ff
waleedhesham446 <waleedhesham2001@gmail.com>
2022-02-11 15:20:57 +0200

    Created Post model

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { userSchema } = require('./User.js');
+
+const postSchema = mongoose.Schema({
+    location: { 
+		type: String,
+		required: true,
+	},
+    time: { 
+		type: Date,
+        required: false,
+        default: new Date(),
+	},
+    caption: { 
+		type: String,
+		required: true,
+	},
+    image: { 
+		type: String,
+		required: true,
+	},
+    commentsCount: { 
+		type: Number,
+		required: false,
+        default: 0,
+	},
+    likesCount: { 
+		type: Number,
+		required: false,
+        default: 0,
+	},
+    allowComments: { 
+		type: Boolean,
+		required: false,
+        default: true,
+	},
+    hashtag: { 
+		type: String,
+		required: true,
+	},
+    privacy: { 
+		type: String,
+		required: true,
+        enum: ['everyone', 'followers', 'onlyMe'],
+	},
+    user: {
+        type: userSchema,
+        required: true,
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = { postSchema, Post };
