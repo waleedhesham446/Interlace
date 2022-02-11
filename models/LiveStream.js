e74e12ca09892a03fd7d565c281a907edc1e9541
waleedhesham446 <waleedhesham2001@gmail.com>
2022-02-11 14:29:06 +0200

    Created LiveStream model

---
+++ models/LiveStream.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const liveStreamSchema = mongoose.Schema({
+    id: { 
+		type: Number,
+		required: true,
+	},
+    watching: { 
+		type: Number,
+		required: false,
+        default: 0,
+	},
+    username: { 
+        type: String,
+        required: true,
+    },
+    url: { 
+        type: String,
+        required: true,
+    },
+    country: { 
+        type: String,
+        required: true,
+    },
+    image: { 
+		type: String,
+		required: true,
+	},
+    coins: { 
+		type: Number,
+		required: true,
+	},
+    stickers: { 
+		type: Number,
+		required: true,
+	},
+});
+
+const LiveStream = mongoose.model('LiveStream', liveStreamSchema);
+
+module.exports = LiveStream;
