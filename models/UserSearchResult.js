6a39a08e2218d47ee6417c3329db2a1b271acf89
waleedhesham446 <waleedhesham2001@gmail.com>
2022-02-11 14:45:26 +0200

    Created UserSearchResult model

---
+++ models/UserSearchResult.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { userSchema } = require('./User.js');
+
+const userSearchResultSchema = mongoose.Schema({
+    iamFollowing: { 
+		type: Boolean,
+		required: true,
+	},
+    user: {
+        type: userSchema,
+        required: true,
+    }
+});
+
+const UserSearchResult = mongoose.model('UserSearchResult', userSearchResultSchema);
+
+module.exports = UserSearchResult;
