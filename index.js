627d8545f579aef5bcf9848aaac62bfc969bab98
waleedhesham446 <waleedhesham2001@gmail.com>
2022-02-14 00:33:03 +0200

    Connected app to routes

--- index.js
+++ index.js
@@ -7,11 +7,35 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const bannerRoutes = require('./routes/banner');
+const chatRoutes = require('./routes/chat');
+const coinsRoutes = require('./routes/coins');
+const fansRoutes = require('./routes/fans');
+const liveStreamRoutes = require('./routes/live_stream');
+const postRoutes = require('./routes/post');
+const rcoinsRoutes = require('./routes/rcoins');
+const stickersRoutes = require('./routes/stickers');
+const userRoutes = require('./routes/user');
+const videosRoutes = require('./routes/videos');
+const vipRoutes = require('./routes/vip');
+
 dotenv.config();
 require('./utilities.js')(io);
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use('/banners', bannerRoutes);
+app.use('/chat', chatRoutes);
+app.use('/coin', coinsRoutes);
+app.use('/fans', fansRoutes);
+app.use('/live', liveStreamRoutes);
+app.use('/post', postRoutes);
+app.use('/rcoin', rcoinsRoutes);
+app.use('/stickers', stickersRoutes);
+app.use('/user', userRoutes);
+app.use('/videos', videosRoutes);
+app.use('/vip', vipRoutes);
 
 app.use('/', (req, res) => {
     res.send('Welcome to Vuexy API');
